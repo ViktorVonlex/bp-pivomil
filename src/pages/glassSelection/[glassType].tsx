be75eaf132cac61c9d139a05c06fdd8113798b4c
ViktorVonlex <suster.ji@gmail.com>
2023-02-27 17:14:26 +0100

    Image from pivomil domain, dynamic glassType

--- src/pages/glassSelection/[glassType].tsx
+++ src/pages/glassSelection/[glassType].tsx
@@ -1,27 +1,57 @@
-import { type NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, type NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Footer from "~/components/Footer";
 
 type Props = {
-  data: {
+  res: {
     glassType: string[]
   }
 }
 
-const Glass: NextPage<Props> = ({data}) => {
+const Glass: NextPage<Props> = ({res}) => {
 
   const [option1, setOption1] = useState<string>("")
   const [nextPage, setNextPage] = useState<string>("/typeSelection")
 
+  const router = useRouter()
+  const {glassType} = router.query;
+  console.log(res)
+
   return (
     <>
-      <div></div>
+      <div>{glassType}</div>
+      <button onClick={()=> history.back()}>hey bitch</button>
+      <Image src={res[2].url} width={50} height={100}/>
       <Footer prevPage="/" nextPage={nextPage} />
     </>
   );
 };
 
 export default Glass;
+
+export const getServerSideProps = ({params}:GetServerSidePropsContext) => {
+
+    const data = {
+        půllitr: [{name: "Džbánek Aspen", price: 89}, {name: "Džbánek Kugel", price: 109}, {name: "Džbánek Prag", price: 99}],
+        třetinka: [{name: "Džbánek Aspen", price: 79}, {name: "Džbánek Kugel", price: 99}, {name: "Džbánek Prag", price: 89, url: "https://www.pivomil.cz/fotky53/fotos/gen320/gen__vyr_1007prag0.jpg"}]
+    }
+
+    let res;
+
+    if (params?.glassType === "třetinku") {
+        res = data.třetinka
+    }
+
+    if(params?.glassType === "půllitr") {
+        res = data.půllitr
+    }
+  
+    return {
+      props: {res}, // will be passed to the page component as props
+    }
+  }
+
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { type NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Footer from "~/components/Footer";
 
@@ -12,6 +14,7 @@ const Home: NextPage<Props> = ({data}) => {
 
   const [option1, setOption1] = useState<string>("")
   const [nextPage, setNextPage] = useState<string>("/typeSelection")
+  const router = useRouter();
 
   return (
     <>
@@ -21,9 +24,13 @@ const Home: NextPage<Props> = ({data}) => {
       <div className="grid grid-rows-3 grid-flow-row grid-cols-2 gap-4 md:gap-8">
         {
           data.options.map(option =>
-          <button className="flex max-w-xs flex-col gap-4 rounded-xl justify-center items-center bg-white/10 p-4 text-white hover:bg-white/20 active:bg-white/20" key={option}>
-            <h2 className="text-2xl font-bold" onClick={() => setOption1(option)}>{option}</h2>
-          </button>
+          <Link href={`/glassSelection/${option.toLowerCase()}`} key={option} className="flex max-w-xs flex-col gap-4 rounded-xl justify-center items-center bg-white/10 p-4 text-white hover:bg-white/20 active:bg-white/20">
+              <button  
+                key={option} onClick={() => setOption1(option)}>
+              <h2 className="text-2xl font-bold">{option}</h2>
+            </button>
+          </Link>
+          
           )
         }
       
