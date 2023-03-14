8c358ccf16a347b0558e06f4e3d964772d931b06
ViktorVonlex <suster.ji@gmail.com>
2023-03-14 20:17:32 +0100

    Wrong import

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -36,7 +36,7 @@ const Dropdown = ({items, selected, setSelected}: Props) => {
             <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                 <Combobox.Input
                 className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                displayValue={(item:Item|Picture) => item.name}
+                displayValue={(item:Item) => item.name}
                 onChange={(event) => setQuery(event.target.value)}
                 />
                 <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
