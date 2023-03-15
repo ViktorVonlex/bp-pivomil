2dd9c7cfd8c65f7fb39825c2239f3f7a22e7769c
ViktorVonlex <suster.ji@gmail.com>
2023-03-15 14:58:04 +0100

    Pictures data added

--- src/pages/glassSelection/typeSelection/picture/index.tsx
+++ src/pages/glassSelection/typeSelection/picture/index.tsx
@@ -3,12 +3,12 @@ import Dropdown from '~/components/Dropdown';
 import { type Size, type Item, type Picture } from '~/util/types';
 import Link from 'next/link';
 import { type NextPage } from 'next';
-import { pictures } from '~/util/pictures';
+import { pictures2 } from '~/util/pictures';
 import ImgHelper from '~/components/ImgHelper';
 import Image from 'next/dist/client/image';
 import { sizes } from '~/util/sizes';
 
-function saveSelectedPictureAndSize(picture:string, size: string, price:number) {
+function saveSelectedPictureAndSize(picture: string, size: string, price: number) {
     const obj = {
         picture: picture,
         size: size,
@@ -19,26 +19,26 @@ function saveSelectedPictureAndSize(picture:string, size: string, price:number) 
 }
 
 const Text: NextPage = () => {
-    const [selected, setSelected] = useState<Picture | undefined>(pictures[0])
+    const [selected, setSelected] = useState<Picture | undefined>(pictures2[0])
     const [selectedSize, setSelectedSize] = useState<Size | undefined>(sizes[0])
     const [loading, setLoading] = useState<boolean>(true)
 
 
     useEffect(() => {
         if (selected !== undefined && selectedSize !== undefined) {
-          saveSelectedPictureAndSize(selected.name, selectedSize.name, selectedSize.price)  
-          setLoading(true)
+            saveSelectedPictureAndSize(selected.name, selectedSize.name, selectedSize.price)
+            setLoading(true)
         }
-    
-      }, [selected])
+
+    }, [selected])
 
 
     return (
         <>
-        <div className="bg-[#AB77AE] w-11/12 sm:w-auto md:p-7 p-5 rounded-lg flex container flex-col items-center">
-           
+            <div className="bg-[#AB77AE] w-11/12 sm:w-auto md:p-7 p-5 rounded-lg flex container flex-col items-center">
+
                 <h2 className="pageHeader text-center sm:text-5xl sm:mb-3 mx-auto">
-                        Vyberte obrázek
+                    Vyberte obrázek
                 </h2>
 
                 <div className="w-64 z-20 mt-3 mx-auto sm:mb-0 sm:w-96">
@@ -50,8 +50,8 @@ const Text: NextPage = () => {
                                     Ukázka
                                 </Link>
                             </div>
-                            <Dropdown selected={selected} setSelected={setSelected as Dispatch<SetStateAction<Item | undefined>>} items={pictures} />
-                        </div>      
+                            <Dropdown selected={selected} setSelected={setSelected as Dispatch<SetStateAction<Item | undefined>>} items={pictures2} />
+                        </div>
                     }
                 </div>
                 <div className="w-64 z-10 mb-3 mx-auto sm:mb-0 sm:w-96">
@@ -62,24 +62,24 @@ const Text: NextPage = () => {
                                 <p className='text-white justify-center items-center p-2'>Velikost:</p>
                             </div>
                             <Dropdown selected={selectedSize} setSelected={setSelectedSize as Dispatch<SetStateAction<Item | undefined>>} items={sizes} />
-                        </div>      
+                        </div>
                     }
                 </div>
                 <div className="w-72 mt-3 sm:mt-4 md:mt-6 flex justify-center">
                     <div className="w-fit flex justify-center items-center bg-white/30 pr-5 rounded-md">
                         <div className="w-28 h-36 relative">
                             {
-                            loading === true && selected?.url !== undefined
-                                ? <>
-                                <Image src={"/blurred-bg.jpg"} alt="Nacitani" fill className="rounded-l-md" />
-                                <ImgHelper url={selected?.url} name={selected?.name} setLoading={setLoading} />
-                                </>
+                                loading === true && selected?.url !== undefined
+                                    ? <>
+                                        <Image src={"/blurred-bg.jpg"} alt="Nacitani" fill className="rounded-l-md" />
+                                        <ImgHelper url={selected?.url} name={selected?.name} setLoading={setLoading} />
+                                    </>
 
-                                : <>
-                                {selected?.url !== undefined &&
-                                    <ImgHelper url={selected?.url} name={selected?.name} setLoading={setLoading} />
-                                }
-                                </>
+                                    : <>
+                                        {selected?.url !== undefined &&
+                                            <ImgHelper url={selected?.url} name={selected?.name} setLoading={setLoading} />
+                                        }
+                                    </>
                             }
                         </div>
                         <div className="ml-5">
@@ -88,8 +88,8 @@ const Text: NextPage = () => {
                         </div>
                     </div>
                 </div>
-            
-        </div>
+
+            </div>
             <div className="inline-flex">
                 <button onClick={() => history.back()} className="bg-gray-300 hover:bg-gray-400 text-gray-800 w-16 h-16 font-bold py-2 px-4 rounded-l">
                     Zpět
@@ -103,7 +103,7 @@ const Text: NextPage = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                     </svg>
                 </Link >
-            </div>    
+            </div>
         </>
     )
 }
